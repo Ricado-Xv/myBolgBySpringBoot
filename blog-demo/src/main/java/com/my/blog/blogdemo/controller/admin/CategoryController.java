918e1bc819c8b46051c7219400edb8bf5a09b31e
Richardo <529319145@qq.com>
2021-08-31 20:34:42 +0800

    这是一次无意义的提交

--- blog-demo/src/main/java/com/my/blog/blogdemo/controller/admin/CategoryController.java
+++ blog-demo/src/main/java/com/my/blog/blogdemo/controller/admin/CategoryController.java
@@ -9,24 +9,32 @@ import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
-
-
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
+/**
+ * @author 13
+ * @qq交流群 796794009
+ * @email 2449207463@qq.com
+ * @link http://13blog.site
+ */
 @Controller
 @RequestMapping("/admin")
 public class CategoryController {
+
     @Resource
     private CategoryService categoryService;
 
     @GetMapping("/categories")
-    public String categoryPage(HttpServletRequest request){
-        request.setAttribute("path","categories");
+    public String categoryPage(HttpServletRequest request) {
+        request.setAttribute("path", "categories");
         return "admin/category";
     }
-    //分类列表
+
+    /**
+     * 分类列表
+     */
     @RequestMapping(value = "/categories/list", method = RequestMethod.GET)
     @ResponseBody
     public Result list(@RequestParam Map<String, Object> params) {
@@ -36,25 +44,31 @@ public class CategoryController {
         PageQueryUtil pageUtil = new PageQueryUtil(params);
         return ResultGenerator.genSuccessResult(categoryService.getBlogCategoryPage(pageUtil));
     }
-    //添加分类
-    @RequestMapping(value = "/categories/save",method = RequestMethod.POST)
+
+    /**
+     * 分类添加
+     */
+    @RequestMapping(value = "/categories/save", method = RequestMethod.POST)
     @ResponseBody
     public Result save(@RequestParam("categoryName") String categoryName,
-                       @RequestParam("categoryIcon") String categoryIcon){
-        if(StringUtils.isEmpty(categoryName)){
-            return ResultGenerator.genFailResult("请输入分类名称!");
+                       @RequestParam("categoryIcon") String categoryIcon) {
+        if (StringUtils.isEmpty(categoryName)) {
+            return ResultGenerator.genFailResult("请输入分类名称！");
         }
-        if(StringUtils.isEmpty(categoryIcon)){
-            return ResultGenerator.genFailResult("请选择分类图标");
+        if (StringUtils.isEmpty(categoryIcon)) {
+            return ResultGenerator.genFailResult("请选择分类图标！");
         }
-        if(categoryService.saveCategory(categoryName,categoryIcon)){
+        if (categoryService.saveCategory(categoryName, categoryIcon)) {
             return ResultGenerator.genSuccessResult();
-        }else {
+        } else {
             return ResultGenerator.genFailResult("分类名称重复");
         }
     }
 
-    //修改分类
+
+    /**
+     * 分类修改
+     */
     @RequestMapping(value = "/categories/update", method = RequestMethod.POST)
     @ResponseBody
     public Result update(@RequestParam("categoryId") Integer categoryId,
@@ -76,7 +90,9 @@ public class CategoryController {
         }
     }
 
-    //详情
+    /**
+     * 详情
+     */
     @GetMapping("/categories/info/{id}")
     @ResponseBody
     public Result info(@PathVariable("id") Integer id) {
@@ -87,7 +103,9 @@ public class CategoryController {
         return ResultGenerator.genSuccessResult(category);
     }
 
-    //删除分类
+    /**
+     * 分类删除
+     */
     @RequestMapping(value = "/categories/delete", method = RequestMethod.POST)
     @ResponseBody
     public Result delete(@RequestBody Integer[] ids) {
@@ -100,4 +118,5 @@ public class CategoryController {
             return ResultGenerator.genFailResult("删除失败");
         }
     }
+
 }
