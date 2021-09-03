e600362be6af5202ab2f0407a156cf9bf8d084aa
Ricado-Xv <57443417+Ricado-Xv@users.noreply.github.com>
2021-09-03 14:07:34 +0800

    Add files via upload

---
+++ all.sql
@@ -0,0 +1,81 @@
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`my_blog_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `my_blog_db`;
+
+
+CREATE TABLE `tb_blog_category` (
+  `category_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '分类表主键',
+  `category_name` varchar(50) NOT NULL COMMENT '分类的名称',
+  `category_icon` varchar(50) NOT NULL COMMENT '分类的图标',
+  `category_rank` int(11) NOT NULL DEFAULT '1' COMMENT '分类的排序值 被使用的越多数值越大',
+  `is_deleted` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否删除 0=否 1=是',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `tb_admin_user` */
+CREATE TABLE `tb_admin_user` (
+  `admin_user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '管理员id',
+  `login_user_name` varchar(50) NOT NULL COMMENT '管理员登陆名称',
+  `login_password` varchar(50) NOT NULL COMMENT '管理员登陆密码',
+  `nick_name` varchar(50) NOT NULL COMMENT '管理员显示昵称',
+  `locked` tinyint(4) DEFAULT '0' COMMENT '是否锁定 0未锁定 1已锁定无法登陆',
+  PRIMARY KEY (`admin_user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*Data for the table `tb_admin_user` */
+/*下面的数据,密码字段对应的是123456的md5加密后的数据*/
+insert  into `tb_admin_user`(`admin_user_id`,`login_user_name`,`login_password`,`nick_name`,`locked`) values (1,'admin','e10adc3949ba59abbe56e057f20f883e','李嘉图',0);
+
+
+DROP TABLE IF EXISTS `tb_blog_tag`;
+
+CREATE TABLE `tb_blog_tag` (
+  `tag_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '标签表主键id',
+  `tag_name` varchar(100) NOT NULL COMMENT '标签名称',
+  `is_deleted` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否删除 0=否 1=是',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`tag_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `tb_blog_tag_relation`;
+
+CREATE TABLE `tb_blog_tag_relation` (
+  `relation_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '关系表id',
+  `blog_id` bigint(20) NOT NULL COMMENT '博客id',
+  `tag_id` int(11) NOT NULL COMMENT '标签id',
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '添加时间',
+  PRIMARY KEY (`relation_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `tb_blog`;
+
+CREATE TABLE `tb_blog` (
+  `blog_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '博客表主键id',
+  `blog_title` varchar(200) NOT NULL COMMENT '博客标题',
+  `blog_sub_url` varchar(200) NOT NULL COMMENT '博客自定义路径url',
+  `blog_cover_image` varchar(200) NOT NULL COMMENT '博客封面图',
+  `blog_content` mediumtext NOT NULL COMMENT '博客内容',
+  `blog_category_id` int(11) NOT NULL COMMENT '博客分类id',
+  `blog_category_name` varchar(50) NOT NULL COMMENT '博客分类(冗余字段)',
+  `blog_tags` varchar(200) NOT NULL COMMENT '博客标签',
+  `blog_status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '0-草稿 1-发布',
+  `blog_views` bigint(20) NOT NULL DEFAULT '0' COMMENT '阅读量',
+  `enable_comment` tinyint(4) NOT NULL DEFAULT '0' COMMENT '0-允许评论 1-不允许评论',
+  `is_deleted` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否删除 0=否 1=是',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '添加时间',
+  `update_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`blog_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `tb_link`;
+CREATE TABLE `tb_link` (
+  `link_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '友链表主键id',
+  `link_type` tinyint(4) NOT NULL DEFAULT '0' COMMENT '友链类别 0-友链 1-推荐 2-个人网站',
+  `link_name` varchar(50) NOT NULL COMMENT '网站名称',
+  `link_url` varchar(100) NOT NULL COMMENT '网站链接',
+  `link_description` varchar(100) NOT NULL COMMENT '网站描述',
+  `link_rank` int(11) NOT NULL DEFAULT '0' COMMENT '用于列表排序',
+  `is_deleted` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否删除 0-未删除 1-已删除',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '添加时间',
+  PRIMARY KEY (`link_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
